fd34372a5979f958f9b929dfee487e267ef7db69
Vimukthi Bandaragoda <vimukthi.nelanga@gmail.com>
2023-08-29 17:21:23 +0530

    nodemailer added

--- mailer/controller/appController.js
+++ mailer/controller/appController.js
@@ -1,5 +1,38 @@
-const signup = (req, res) => {
-  res.status(201).json("Signup Succesfully!");
+const nodemailer = require("nodemailer");
+
+const signup = async (req, res) => {
+  // Testing account
+  let testAccount = await nodemailer.createTestAccount();
+
+  const transporter = nodemailer.createTransport({
+    host: "smtp.forwardemail.net",
+    port: 465,
+    secure: true,
+    auth: {
+      // TODO: replace `user` and `pass` values from <https://forwardemail.net>
+      user: 'REPLACE-WITH-YOUR-ALIAS@YOURDOMAIN.COM',
+      pass: 'REPLACE-WITH-YOUR-GENERATED-PASSWORD'
+    }
+  });
+
+  let message = {
+    from: '"Fred Foo 👻" <foo@example.com>', // sender address
+    to: "bar@example.com, baz@example.com", // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world?", // plain text body
+    html: "<b>Hello world?</b>", // html body
+  };
+
+  transporter
+    .sendMail(message)
+    .then(() => {
+      return res.status(201).json({ msg: "You should receive an email" });
+    })
+    .catch((error) => {
+      return res.status(500).json({ error });
+    });
+
+  // res.status(201).json("Signup Succesfully!");
 };
 
 const getBill = (req, res) => {
@@ -8,5 +41,5 @@ const getBill = (req, res) => {
 
 module.exports = {
   signup,
-  getBill
+  getBill,
 };
